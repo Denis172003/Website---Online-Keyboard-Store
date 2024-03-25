28879874dacc33dff8133daea008eb488ce01f84
Denis172003 <denis.mitica@gmail.com>
2024-03-25 10:24:08 +0200

    commit 1.5

--- index.js
+++ index.js
@@ -13,10 +13,31 @@ console.log("Director de lucru", process.cwd());
 
 app.set("view engine","ejs");
 
+app.get("/", function(req, res) {
+    res.sendFile(__dirname+"/index.html");
+});    
+
 app.use("/resurse", express.static(__dirname+"/resurse"));
 
+app.get("/cerere", function(req, res) {
+    res.send("<b>Cerere primita</b> <span style='color:red;'>world!</span>");
+});
 
+app.get("/data", function(req,res,next){
+    res.write("Data: ");
+    next();
+})
 
+app.get("/data", function(req,res){
+    res.write(""+new Date());
+    res.end();
+})
+    
+
+app.get("/suma/:a/:b", function(req,res){
+    var suma=parseInt(req.params.a)+parseInt(req.params.b)
+    res.send(""+suma);
+})
 
 
 
